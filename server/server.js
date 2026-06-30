31b86e4df8f0893eb95087bdfd707869da3891c2
ajaniobatofolasewa-dev <ajaniobatofolasewa@gmail.com>
2026-06-30 13:46:34 +0100

    some changes made

--- server/server.js
+++ server/server.js
@@ -3,52 +3,44 @@ require("dotenv").config();
 const crypto = require("crypto");
 const cors = require("cors");
 const { config } = require("dotenv");
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-
-// const path = require('path');
-
-// // 1. Tell Express where your static frontend files live
-// app.use(express.static(path.join(__dirname, '../client')));
-
-// // 2. The Wildcard Route fallback for index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client', 'index.html'));
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
 // });
+
+
 
 const PORT = process.env.PORT || 7700;
 
 
 app.post("/api/hash", (req, res) => {
   const { text, algorithm } = req.body;
-
+  
   if (!text || !algorithm) {
     return res
-      .status(400)
+    .status(400)
       .json({ error: "Missing plain text or hashing algorithm" });
   }
-
+  
   try {
     const validAlgors = ["sha256", "sha1", "md5"];
 
     if (!validAlgors.includes(algorithm.toLowerCase())) {
       return res.status(400).json({ error: "Unsupported algorithm type" });
     }
-
+    
     const calculatedHash = crypto
-      .createHash(algorithm)
+    .createHash(algorithm)
       .update(text)
       .digest("hex");
-
-    return res.json({ hash: calculatedHash });
+      
+      return res.json({ hash: calculatedHash });
   } catch (error) {
     console.error(error + "the error message sha");
     return res
@@ -65,21 +57,21 @@ app.post("/api/verify", (req, res) => {
   try {
     if (!text || !expectedHash || !algorithmProvided) {
       return res
-        .status(400)
+      .status(400)
         .json({ error: "fields are empty or algorithm not selected" });
-    }
-
+      }
+      
     const validAlgos = ["sha256", "sha1", "md5"];
     if (!validAlgos.includes(algorithmProvided.toLowerCase())) {
       return res
-        .status(400)
-        .json({ error: "Unsupported or invalid algorithm selected." });
+      .status(400)
+      .json({ error: "Unsupported or invalid algorithm selected." });
     }
 
     const rawText_toHash = crypto
-      .createHash(algorithmProvided)
-      .update(text)
-      .digest("hex");
+    .createHash(algorithmProvided)
+    .update(text)
+    .digest("hex");
 
     if (rawText_toHash !== expectedHash) {
       return res.json({ match: false });
@@ -92,6 +84,14 @@ app.post("/api/verify", (req, res) => {
 });
 
 // app.use(express.static(path.join(__dirname, '../client')))
+
+// 1. Tell Express where your static frontend files live
+app.use(express.static(path.join(__dirname, '../client')));
+
+// 2. The Wildcard Route fallback for index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Backend active on server http://localhost:${PORT}`);
